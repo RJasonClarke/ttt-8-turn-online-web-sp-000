a08febf02201022cdc6f34db5a55832a997abd5f
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-18 23:18:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,5 +8,9 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  index.between(0, 8)
+  index.between(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
 end
