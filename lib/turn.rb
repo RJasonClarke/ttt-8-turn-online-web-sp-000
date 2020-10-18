17f8a403ba289f04ba3f5891e9255e025a2ec8cb
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-18 23:38:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,54 +8,25 @@ WIN_COMBINATIONS =
 [0,4,8],
 [2,4,6]]
 
-#
-# #### `#display_board`
-# Define a method that prints the current board representation based on the `board` argument passed to the method.
 def display_board(board)
 puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 return " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# #### `#input_to_index`
-# Once the user inputs where they would like to go on the board, we then have to convert this to the board index
-# multiple times. Instead of doing that in a lot of places, we can remove this repetitive code and put it in this
-# helper method. This method takes the user_input (which is a string), converts it to an Integer, and subtracts 1.
-# Remember that, from the player's point of view, the board contains spaces 1-9. But, an array's indexes start their
-# count at 0.
-
-#Take players 1-9 input and turn it into a board input 0-8
 def input_to_index(input)
   return index = input.to_i - 1
 end
 
-# #### `#move`
-# Your `#move` method must take in three arguments: **1)** the board array, **2)** the index in the board array that
-# the player would like to fill out with an "X" or and "O", and **3)** the player's character (either "X" or "O").
-# We previously had you write this method with a default argument of "X" for the third argument, but that is no
-# longer needed.
-# create a move for player
 def move(board, index, character)
   board[index] = character
   turn_count(board)
   display_board(board)
 end
 
-# #### `#position_taken?`
-# The `#position_taken?` method will be responsible for evaluating the position selected by the user against the
-# Tic Tac Toe board and checking to see whether or not that index on the board array is occupied. If the user would
-# like to fill out position 1, our `#position_taken?` method will check to see if that board index is vacant or if it
-# contains an "X" or an "O". If the position is free, the method should return `false` (i.e. "not taken"), otherwise
-# it will return `true`.
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# #### `#valid_move?`
-# Build a method `valid_move?` that accepts a board and an index to check and
-# returns `true` if the move is valid and `false` or `nil` if not. A valid move means that the submitted position is:
-# 1. Present on the game board.
-# 2. Not already filled with a token.
-#Check for whether the input is a valid move
 def valid_move?(board, index)
 index.between?(0,8) && !position_taken?(board, index)
 end
