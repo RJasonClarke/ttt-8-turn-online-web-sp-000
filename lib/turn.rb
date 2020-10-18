c6e2d49152bbd07d09e9e674dd026b394a8cb32c
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-18 23:11:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,10 @@ def input
   gets.chomp
 end
 
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
